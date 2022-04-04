3a41bb3370fc9f8084575b74c4526e8690de79d6
theophildio <dtheo.sangma@gmail.com>
2022-04-04 15:01:06 +0600

    Review item added into Reviews Route and UI

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import useReviews from "../../hooks/useReviews";
+import "./Home.css";
 
 const Home = () => {
-  return (
-    <div className='home-container'>
-      <div className="hero-img">
-        <img src="watch.png" alt="Apple Watch" />
-      </div>
-      <div className="hero-content">
-        <h2>Belive in Technology, Make the change</h2>
-        <p>Just put on your Apple Watch and take off. Series 7 keeps all the things you care about front and center with its larger screen. The larger display enhances the entire experience, making Apple Watch easier to use and read. Series 7 represents our biggest and brightest thinking.</p>
-        <button className='hero-btn'>buy now</button>
-      </div>
-
-    </div>
-  );
+  const [reviews] = useReviews();
+	return (
+		<div className="home-container">
+			<section className="hero">
+				<div className="hero-img">
+					<img src="watch.png" alt="Apple Watch" />
+				</div>
+				<div className="hero-content">
+					<h2>Belive in Technology, Make the change</h2>
+					<p>
+						Just put on your Apple Watch and take off. Series 7 keeps all the
+						things you care about front and center with its larger screen. The
+						larger display enhances the entire experience, making Apple Watch
+						easier to use and read. Series 7 represents our biggest and
+						brightest thinking.
+					</p>
+					<button className="hero-btn">buy now</button>
+				</div>
+			</section>
+			<main>
+				<section className="customer-reviews">
+					<h3>
+						customer <span>reviews</span>
+					</h3>
+          <p>Reviews: {reviews.length}</p>
+				</section>
+			</main>
+		</div>
+	);
 };
 
 export default Home;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import useReviews from "../../hooks/useReviews";
+import ReviewCard from "../Review/ReviewCard";
+import "./Reviews.css";
 
 const Reviews = () => {
-  return (
-    <div>
-      <h2>this is review</h2>
-    </div>
-  );
+	const [reviews] = useReviews();
+	return (
+		<div className="reviews-container">
+			<h3>
+				customer <span>reviews</span>
+			</h3>
+			<div className="reviews">
+				{reviews.map((review) => (
+					<ReviewCard key={review.id} review={review}></ReviewCard>
+				))}
+			</div>
+		</div>
+	);
 };
 
-export default Reviews;<h2>this is review</h2>
+export default Reviews;
+<h2>this is review</h2>;
