d65b329ea4984d4c4abd4d05c0b9970070def568
theophildio <dtheo.sangma@gmail.com>
2022-04-04 15:04:33 +0600

    fakeData and custom hook added in public folder and src folder

--- src/hooks/useReviews.js
+++ src/hooks/useReviews.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 const useReviews = () => {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
-    fetch('fakeDB.json')
+    fetch('fakeData.json')
     .then(res => res.json())
     .then(data => setReviews(data));
   }, []);
