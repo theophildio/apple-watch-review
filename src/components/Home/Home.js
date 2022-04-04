e07710f863874a116becb006305a6b2ce8041e9a
theophildio <dtheo.sangma@gmail.com>
2022-04-04 11:41:11 +0600

    Hero section added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <h2>this is home</h2>
+    <div className='home-container'>
+      <div className="hero-img">
+        <img src="watch.png" alt="Apple Watch" />
+      </div>
+      <div className="hero-content">
+        <h2>Belive in Technology, Make the change</h2>
+        <p>Just put on your Apple Watch and take off. Series 7 keeps all the things you care about front and center with its larger screen. The larger display enhances the entire experience, making Apple Watch easier to use and read. Series 7 represents our biggest and brightest thinking.</p>
+        <button className='hero-btn'>buy now</button>
+      </div>
     </div>
   );
 };
